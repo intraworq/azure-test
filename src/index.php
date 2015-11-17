d81169031f8ebf99f3bf1de34d33eab2e0b49808
Bolesław Tekielski <bt@intraworq.com>
2015-11-17 15:38:36 +0100

    Initial import

---
+++ src/index.php
@@ -0,0 +1,15 @@
+<?php
+
+require __DIR__ . '/../vendor/autoload.php';
+
+$app = new \Slim\Slim();
+
+$app->get('/', function () {
+    echo "Hello, Azure";
+});
+
+$app->get('/hello/:name', function ($name) {
+    echo "Hello, $name";
+});
+
+$app->run();
