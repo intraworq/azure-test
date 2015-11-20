3d77a2f322b848cd5360dd1dd45790ae9a17d188
Bolesław Tekielski <bt@intraworq.com>
2015-11-20 23:40:12 +0100

    Added Twig based view

--- src/IntraworQ/index.php
+++ src/IntraworQ/index.php
@@ -2,14 +2,29 @@
 
 require __DIR__ . '/../../vendor/autoload.php';
 
-$app = new \Slim\Slim();
+$app = new \Slim\Slim([
+		'mode' => 'development',
+		'view' => new \Slim\Views\Twig(),
+		'templates.path' => __DIR__ . '/Views'
+	]);
 
-$app->get('/', function () {
-    echo "Hello, Azure";
+$view = $app->view();
+
+$view->parserOptions = [
+		'debug' => true,
+		'cache' => __DIR__ . '/../../cache'
+	];
+
+$view->parserExtensions = [
+		new \Slim\Views\TwigExtension()
+	];
+
+$app->get('/', function () use($app) {
+	$app->render('index.html', ['location' => 'Azure']);
 });
 
 $app->get('/hello/:name', function ($name) {
-    echo "Hello, $name";
+	echo "Hello, $name";
 });
 
 $app->run();
